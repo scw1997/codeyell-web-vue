7fd2a105e9d6df3ea2eec1b535ba613d36775e74
sunchuanwen <sunchuanwen@union400.com>
2023-12-31 19:37:10 +0800

    config: 初始化

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router';
-
+import {Input} from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 const app = createApp(App);
 
 app.use(router);
+
+app.use(Input);
 
 app.mount('#root');
