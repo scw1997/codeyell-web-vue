a754edc472f01273d59bb864cc450086edda95d0
sunchuanwen <gouri123>
2024-04-02 10:57:03 +0800

    config: 重构2.0

--- config/secywo.dev.ts
+++ config/secywo.dev.ts
@@ -1,11 +1,12 @@
 //secywo脚手架自dev环境自定义配置
 
 import routes, { devRoutes } from './routes';
+import { defineConfig } from 'secywo-template-cli';
 const env = process.env.SECYWO_ENV;
 console.log('env', env);
-export default {
+export default defineConfig('dev', {
     router: {
         type: 'browser',
         routes: [...(env === 'dev' ? devRoutes : []), ...routes]
     }
-};
+});
