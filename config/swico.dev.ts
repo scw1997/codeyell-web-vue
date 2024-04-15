83db44213a0c055360ce6b3ff190768959dbe445
sunchuanwen <gouri123>
2024-04-15 18:08:33 +0800

    config: 重构2.0

--- config/swico.dev.ts
+++ config/swico.dev.ts
@@ -2,9 +2,8 @@
 
 import routes, { devRoutes } from './routes';
 import { defineConfig } from 'swico';
-const env = process.env.SWICO_ENV;
 export default defineConfig('dev', {
     router: {
-        routes: [...(env === 'dev' ? devRoutes : []), ...routes]
+        routes: [...routes, ...devRoutes]
     }
 });
